5e24a3af572dd396532f8d876461d2a23ac1b3b5
Wei Wei <Wei@WEI-XPS>
2016-07-24 12:15:33 -0600

    Added chapter 18

---
+++ Chapter18/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter18
+{
+    class Program
+    {
+        public const int Repetitions = 1000;
+
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = DoWork;
+            Thread thread = new Thread(threadStart);
+            //thread.Priority = ThreadPriority.Highest;
+            thread.Start();
+            for (int count = 0; count < Repetitions; count++)
+            {
+                Console.Write("-");
+            }
+            thread.Join();
+        }
+
+        public static void DoWork()
+        {
+            for (int count = 0; count < Repetitions; count++)
+            {
+                Console.Write("+");
+            }
+        }
+    }
+}
